36ec4fc112da6132c1f92b0fb44f8de8798f5ce0
Ramakrishna A <aruvaramakrishna@gmail.com>
2026-05-25 10:04:33 +0530

    removed provider secrets

--- Terraform/Modules/remote/providers.tf
+++ Terraform/Modules/remote/providers.tf
@@ -12,8 +12,8 @@ terraform {
 provider "azurerm" {
   features {}
 
-  client_id       = "35b37932-7b61-44a2-bc5f-d40b4ce53dcf"
-  client_secret   = ""
-  tenant_id       = "459865f1-a8aa-450a-baec-8b47a9e5c904"
-  subscription_id = "2e28c82c-17d7-4303-b27a-4141b3d4088f"
+  client_id       = null
+  client_secret   = null
+  tenant_id       = null
+  subscription_id = null
 }
